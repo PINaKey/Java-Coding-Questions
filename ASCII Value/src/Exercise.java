a6a6edeb879ef0811f46e1d1e32041fb6c309e25
Pinaki Bandyopadhyay <pinaki@Pinakis-MacBook-Air.local>
2026-03-29 16:58:49 +0530

    ASCII value

---
+++ ASCII Value/src/Exercise.java
@@ -0,0 +1,12 @@
+public class Exercise {
+
+    // Function to find the ASCII value of a given character
+    public static int findAsciiValue(char character) {
+        return (int) character;
+    }
+
+    // Function to return the ASCII value of a character
+    public static int getAsciiValue(char character) {
+        return findAsciiValue(character);
+    }
+}
